e7dbad3b466e26ee07828ce8c3f0ebd4a7f412e2
Merle Makoben <merlemakoben@gmail.com>
2022-02-18 17:10:00 +0100

    Add JS

---
+++ js/answers.js
@@ -0,0 +1,16 @@
+//const correctAnswer = document.querySelector('[data-js="correct"]')
+
+function wrongAnswers() {
+  const wrongAnswer = document.querySelector('[data-js="incorrect"]');
+
+    wrongAnswer.addEventListener('click', () => {
+      wrongAnswer.classList.add('card__answer-button--incorrect');
+    });
+  });
+
+}
+
+export default wrongAnswers;
+
+// corretAnswer.addEventListener('click', () => {
+// correctAnswer.classList.add('card__answer-button--correct');
